3bc3deede0fa8794fe03f134461d64d2df19ddf3
Mariam <mariamm.marwan@gmail.com>
2023-12-19 11:55:35 -0800

    alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,10 +7,10 @@ int main(void)
 {
 	char n;
 
-	for (n = 'a', n <= 'z', n++)
+	for (n = 'a'; n <= 'z'; n++)
 		putchar(n);
 
-	for (n = 'A', n <= 'Z', n++)
+	for (n = 'A'; n <= 'Z'; n++)
 		putchar(n);
 
 	putchar('\n');
