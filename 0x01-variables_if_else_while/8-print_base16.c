a65146f1d26b650d088aaf819d293035c1f0fa35
Mariam <mariamm.marwan@gmail.com>
2023-12-19 12:35:28 -0800

    base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	char a;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar((n % 10) + '0')
+	}
+	for (a = 'a'; a <= 'f'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
