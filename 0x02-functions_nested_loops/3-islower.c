ec9b109ec3188013c2cffe911b8a8ad1aa9cee70
Mariam <mariamm.marwan@gmail.com>
2023-12-20 05:50:26 -0800

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - Entry point
+ * @c: checker
+ * Return: 0
+ */
+int _islower(int c)
+{
+	char c;
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
